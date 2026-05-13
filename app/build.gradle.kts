92bb2808374959726c225e1c9423809159553f40
Vinod Abhishek H <vinodabhishek.sea@gmail.com>
2026-05-13 08:59:28 +0530

    Phase 10 - Mandi Watch price graph complete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
